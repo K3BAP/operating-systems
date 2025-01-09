74e4591bf0ad9909f2186d12f5aa683937dac6d2
Fabian Sponholz <f.sponholz00@gmail.com>
2025-01-09 18:19:14 +0100

    Adjust code from 3B to run in different docker containers (docker compose)

---
+++ portfolio2/aufgabe4/Main.Dockerfile
@@ -0,0 +1,6 @@
+FROM gradle:jdk21-alpine
+WORKDIR /app
+COPY src src
+COPY build.gradle .
+RUN gradle build
+CMD ["gradle", "run"]
---
+++ portfolio2/aufgabe4/src/main/java/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    private static final int recursions = 10000;
+    private static final int repeatExperimentCount = 1000;
+
+    private static List<Long> minimumLatencies;
+
+    public static void main(String[] args) throws Exception {
+        minimumLatencies = new ArrayList<>();
+        System.out.println("Running " + repeatExperimentCount + " experiments with " + recursions + " recursions each.");
+        Experiment experiment = new Experiment(recursions);
+
+        for (int i = 0; i < repeatExperimentCount; i++) {
+            System.out.println("Running experiment " + (i + 1) + "/" + repeatExperimentCount);
+            long min = experiment.runExperiment();
+            minimumLatencies.add(min);
+        }
+        experiment.finish();
+        exportLatencies(minimumLatencies, "/mnt/latencies.txt");
+    }
+
+    private static void exportLatencies(List<Long> latencies, String filename) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+            for (Long latency : latencies) {
+                writer.write(latency.toString());
+                writer.newLine();
+            }
+            System.out.println("Latencies exported to " + filename);
+        } catch (IOException e) {
+            System.err.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+
+}
